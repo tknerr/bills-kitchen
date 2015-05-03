7c998e41a7939e9dbf37e8547c597c6a6c3bbab2
Torben Knerr <mail@tknerr.de>
2015-05-04 01:06:03 +0200

    don't use a predefined proxy, and warn the user if it's not configured

--- files/tools/scripts/proxy-on.bat
+++ files/tools/scripts/proxy-on.bat
@@ -5,11 +5,18 @@
 set BK_USE_PROXY=1
 
 :: set the proxy on the host system
-set HTTP_PROXY=http://10.12.1.230:8083
-set HTTPS_PROXY=https://10.12.1.230:8083
+set HTTP_PROXY=http://[your.proxy]:[port]
+set HTTPS_PROXY=https://[your.proxy]:[port]
 set NO_PROXY=localhost,127.0.0.1,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16
 
 :: reuse the host proxy in vagrant VMs
 set VAGRANT_HTTP_PROXY=%HTTP_PROXY%
 set VAGRANT_HTTPS_PROXY=%HTTPS_PROXY%
 set VAGRANT_NO_PROXY=%NO_PROXY%
+
+
+:: better check :-)
+if "%HTTP_PROXY%" == "http://[your.proxy]:[port]" (
+  echo oops - did you forget to configure the proxy in %~df0?
+  exit /b 1
+)
