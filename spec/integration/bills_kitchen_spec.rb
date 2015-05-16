3415a99d217ae5094e6cbf7a89ba69b124ee0e7d
Torben Knerr <mail@tknerr.de>
2015-05-16 23:32:46 +0200

    fix detection of installed atom version in the rspec tests

--- spec/integration/bills_kitchen_spec.rb
+++ spec/integration/bills_kitchen_spec.rb
@@ -46,7 +46,7 @@ describe "bills kitchen" do
       # see https://github.com/atom/atom-shell/issues/683
       # so we 1) ensure the atom.cmd is on the PATH and 2) it's the right version
       cmd_succeeds "#{BUILD_DIR}/tools/atom/Atom/resources/cli/atom.cmd -v"
-      cmd_succeeds "grep '0.199.0' #{BUILD_DIR}/tools/atom/Atom/resources/app/package.json"
+      cmd_succeeds "grep 'Package: atom@0.199.0' #{BUILD_DIR}/tools/atom/Atom/resources/LICENSE.md"
     end
     it "installs apm 0.167.0" do
       run_cmd("#{BUILD_DIR}/tools/atom/Atom/resources/app/apm/bin/apm.cmd -v").should match('0.167.0')
